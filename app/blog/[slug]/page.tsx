b9afa9f142828e17102227cabe83966c63dc0e27
Pham Minh Thi <thi.pham@rowboatsoftware.com>
2025-12-18 12:15:03 +0700

    test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: Params) {
 
 export default function Page({ params }: Params) {
   return <>
-  <h1>Slug: {params.slug}</h1>
+  <h1>Slug : {params.slug}</h1>
   <p>Hello world</p>
   </>;
 }
